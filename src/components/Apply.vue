afe652dfc3109addec08f5a85a97ec09074e294e
Dima Polskoy <polskoydm@gmail.com>
2023-06-25 14:22:39 -0600

    add

--- src/components/Apply.vue
+++ src/components/Apply.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="apply-container">
-      <h1 style="color: black">Apply for {{ job ? job.title : 'Job' }}</h1>
-  
+      <h1 style="color: black">Apply for {{ job && job.title }}</h1><br>
+     
+       <p style="color: black">{{ job && job.description }} </p>
+       <a v-if="job && job.link" :href="job.link" style="color: rgb(63, 63, 255)">Show More</a>
+
+       <br>       <br>
       <form class="apply-form">
         <div class="form-group">
           <label for="name">Name</label>
@@ -15,6 +19,10 @@
           <label for="resume">Resume</label>
           <input type="file" class="form-control" id="resume" v-on:change="handleResumeUpload" />
         </div>
+        <div class="form-group">
+        <label for="cover">Cover Letter</label>
+        <textarea class="form-control" id="cover" v-model="cover"></textarea>
+      </div>
         <button type="button" class="btn btn-primary" @click="submitApplication">Submit</button>
       </form>
   
@@ -31,9 +39,12 @@
       return {
         jobs: [],
         job: null,
+        link: null,
+        description: null,
         name: '',
         email: '',
         resume: null,
+        cover: '',
         message: '', // To store the application status message
       };
     },
@@ -61,22 +72,29 @@
         formData.append('name', this.name);
         formData.append('email', this.email);
         formData.append('resume', this.resume);
+        formData.append('cover', this.cover);
+        formData.append('title', this.job.title); // Add the job title to the form data
+        formData.append('type', this.job.type); // Add the job type to the form data
   
         axios
           .post('https://polskoydm.pythonanywhere.com/submit', formData)
           .then(response => {
             this.message = response.data.message;
+            this.resetForm(); // Clear the form fields
           })
           .catch(error => {
             console.error(error);
           });
       },
+      resetForm() {
+        this.name = '';
+        this.email = '';
+        this.resume = '';
+        this.cover = '';
+      },
     },
   };
   </script>
-  
- 
-  
   
 <style scoped>
 .message {

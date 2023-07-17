db69fcd3ea7317ccb49ec52c7f246561fb884e38
Dima Polskoy <polskoydm@gmail.com>
2023-07-17 01:45:23 -0700

    test

---
+++ src/components/WellKnown.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <pre>{{ fileContent }}</pre>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        fileContent: '',
+      };
+    },
+    mounted() {
+      this.fetchFileContent();
+    },
+    methods: {
+      fetchFileContent() {
+        fetch('/.well-known/apple-developer-merchantid-domain-association')
+          .then(response => response.text())
+          .then(data => {
+            this.fileContent = data;
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      },
+    },
+  };
+  </script>
+  
--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import About from './components/About.vue';
 import News from './components/News.vue';
 import Apply from './components/Apply.vue';
 import Payment from './components/Payment.vue';
+import WellKnown from './components/WellKnown.vue';
 import Features from './components/Features.vue';
 
 
@@ -14,6 +15,11 @@ const routes = [
     path: '/features',
     name: 'Features',
     component: Features,
+  },
+  {
+    path: '/.well-known',
+    name: 'WellKnown',
+    component: WellKnown, 
   },
   {
     path: '/apply/:id',

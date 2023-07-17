a6a658cea6d40f8ae2e6009ec1a568130d738b10
Dima Polskoy <polskoydm@gmail.com>
2023-07-17 01:52:24 -0700

    up

--- src/components/WellKnown.vue
+++ src/components/WellKnown.vue
@@ -1,31 +1,40 @@
-<template>
+
+  <template>
     <div>
-      <pre>{{ fileContent }}</pre>
+      <button @click="downloadFile">Download File</button>
     </div>
   </template>
   
   <script>
   export default {
-    data() {
-      return {
-        fileContent: '',
-      };
-    },
-    mounted() {
-      this.fetchFileContent();
-    },
     methods: {
-      fetchFileContent() {
-        fetch('/.well-known/apple-developer-merchantid-domain-association')
-          .then(response => response.text())
-          .then(data => {
-            this.fileContent = data;
-          })
-          .catch(error => {
-            console.log(error);
-          });
+      downloadFile() {
+        const fileUrl = '/.well-known/apple-developer-merchantid-domain-association'; // Replace with the actual file URL
+  
+        // Create an anchor element
+        const link = document.createElement('a');
+        link.href = fileUrl;
+        link.download = 'filename.ext'; // Replace with the desired filename and extension
+        link.target = '_blank'; // Open the file in a new tab/window if needed
+        link.rel = 'noopener noreferrer';
+  
+        // Trigger the download
+        link.click();
       },
     },
   };
   </script>
+  In this example, when the "Download File" button is clicked, the downloadFile method is called. Inside this method, an anchor element (<a>) is dynamically created. The href attribute is set to the URL of the file you want to download. The download attribute specifies the desired filename and extension for the downloaded file. Adjust these attributes according to your file's location and desired filename.
+  
+  Finally, the click event is programmatically triggered on the anchor element, initiating the file download.
+  
+  Remember to replace /path/to/file with the actual URL of the file you want to download and adjust the download attribute to set the desired filename and extension.
+  
+  By using this approach, when the user clicks the button, the file will be downloaded to their local machine.
+  
+  
+  
+  
+  
+  
   

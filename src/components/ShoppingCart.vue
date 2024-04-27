8f7abc743b7e620fbc7c93daa8fd9c84c1ced3bb
Dmitry Polskoy <96145205+AAAxis@users.noreply.github.com>
2024-04-27 12:27:55 -0600

    config

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -87,7 +87,16 @@ export default {
   },
   created() {
     const token = window.location.href.split('/')[3];
-    const firebaseConfig = { /* Firebase config */ };
+    const firebaseConfig = { 
+
+        apiKey: "AIzaSyASwq11lvLT6YfaGwp7W_dCBICDzVsBbSM",
+ authDomain: "bankapp-9798a.firebaseapp.com",
+ projectId: "bankapp-9798a",
+ storageBucket: "bankapp-9798a.appspot.com",
+ messagingSenderId: "868698601721",
+ appId: "1:868698601721:web:e061dcefcb437f53854a28",
+ measurementId: "G-WY7R44DDM4"
+     };
     firebase.initializeApp(firebaseConfig);
 
     const db = firebase.firestore();

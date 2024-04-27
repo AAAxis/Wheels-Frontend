db15ed6463c56371121018e1ea6879e34d1a7243
Dmitry Polskoy <96145205+AAAxis@users.noreply.github.com>
2024-04-27 13:02:27 -0600

    default

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -120,13 +120,14 @@ export default {
     updateOrder() {
       // Initialize Firebase
       const firebaseConfig = {
-        apiKey: "YOUR_API_KEY",
-        authDomain: "YOUR_AUTH_DOMAIN",
-        projectId: "YOUR_PROJECT_ID",
-        storageBucket: "YOUR_STORAGE_BUCKET",
-        messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-        appId: "YOUR_APP_ID",
-        measurementId: "YOUR_MEASUREMENT_ID"
+   
+        apiKey: "AIzaSyASwq11lvLT6YfaGwp7W_dCBICDzVsBbSM",
+ authDomain: "bankapp-9798a.firebaseapp.com",
+ projectId: "bankapp-9798a",
+ storageBucket: "bankapp-9798a.appspot.com",
+ messagingSenderId: "868698601721",
+ appId: "1:868698601721:web:e061dcefcb437f53854a28",
+ measurementId: "G-WY7R44DDM4"
       };
       firebase.initializeApp(firebaseConfig);
 
